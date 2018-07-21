e6ffd7eb25f5bcd74e26861a385ad05e8e67add4
abhishekbvs <bvsabhishek@gmail.com>
2018-07-21 19:14:00 +0530

    Daily Challenge 2

---
+++ Day2.java
@@ -0,0 +1,14 @@
+//Daily Challenge
+//Question No: 2
+//Link:http://codeforces.com/problemsets/acmsguru/problem/99999/403
+//Tags: Basic Math 
+//Difficulty: Easy 
+//Deadline: 21-07-18 11:00pm
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int k = x.nextInt();
+		System.out.println(2*k+1);
+	}
+}
