5c118b612cad091abc199cd1a0980d58f8094b33
Saibhargavkarnati <43804395+Saibhargavkarnati@users.noreply.github.com>
2018-10-03 11:44:50 +0530

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -2,13 +2,13 @@
 //Question No: 2
 //Link:http://codeforces.com/problemsets/acmsguru/problem/99999/403
 //Tags: Basic Math 
-//Difficulty: Easy 
-//Deadline: 21-07-18 11:00pm
+ 
+
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Scanner x = new Scanner(System.in);
-		int k = x.nextInt();
-		System.out.println(2*k+1);
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(2*n+1);
 	}
 }
